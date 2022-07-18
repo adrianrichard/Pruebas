6df4e92fea038d81706c03f3f4630f6f36b91e49
unknown <rodrigoadrianrichard@gmail.com>
2022-07-17 23:43:26 -0300

    Nada2

--- pruebaVSC.py
+++ pruebaVSC.py
@@ -1,14 +1,1 @@
-import wx
-
-class MiAplicacion(wx.Frame):
-    def __init__(self,parent,title):
-        wx.Frame.__init__(self,parent=parent,title=title,size=(300,200))
-        
-        self.Centre(True)
-        self.Show()
-        
-
-if __name__=='__main__':
-    app = wx.App()
-    frame = MiAplicacion(None,u"Mi primera aplicación")
-    app.MainLoop()
+print("salida")
