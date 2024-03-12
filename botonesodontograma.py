712ac0c779cd5d1078b6567b58b701c508b402e3
Adrian Richard <109044908+adrianrichard@users.noreply.github.com>
2024-03-12 12:29:38 -0300

    botones, en 90 %

--- botonesodontograma.py
+++ botonesodontograma.py
@@ -1,54 +1,205 @@
 import tkinter as tk
 
-
-
 root = tk.Tk()
 #root.configure(padx = 10, pady = 10)
 buttons = []
-colores=["red", "yellow", "blue","green"]
+colores=["red", "yellow", "blue","white"]
 #tk.Button(root,height=6, width=6, justify="left").pack(padx=0, pady=0)
-canvas = tk.Canvas(root, width=300, height=300)
+ancho = 900
+canvas = tk.Canvas(root, width=ancho, height=600)
 canvas.pack()
 
 
-def button_click(event, index):
-    canvas.itemconfig(buttons[index], fill="red")
+#def button_click(event, index):
+    #canvas.itemconfig(buttons[index], fill="red")
 
 def clicked():
     print("You clicked play!")
     #color="purple"
 
 def crear_dientes():
-    width = 60
-    height = 60
+    width = 42
+    height = 42
     padding = 10
-    num_buttons = 5
-    dist_y =0
-    #tk.Button(root,height=6, width=6, justify="left", command=clicked).pack(padx=0, pady=0)
+    num_buttons = 8
+    x1=0
     for i in range(num_buttons):
-        dist_y +=10
-        x1 = i * (width)
-        y1 = 0
+        x1 = x1 + padding
+        y1 = padding
         x2 = x1 + width
         y2 = y1 + height
 
-    ##    print(y2)
-        #button_text = f"Button {i+1}"
-        #button = canvas.create_rectangle(x1, y1, x2, y2, fill="lightgray")
-    ##    canvas.create_rectangle(x1, y1, x2, y2,fill="red", tags="playbutton")
-    
-        btn = tk.Button(root, text='Welcome to Tkinter!', width=3, height=3, bd='0')
-        btn.place(x=0, y=0)
-        canvas.create_polygon(x1, y1, x1 + width/2, y2/2, x1, y2,fill=colores[0], outline = "black", tags="d1_i")
-        #canvas.create_polygon(x1, y1, x1 + width/2, y2/2, x2, y1,fill=colores[1], outline = "black", tags="d1_s")
-        #canvas.create_polygon(x2, y1, x1 + width/2, y2/2, x2, y2,fill=colores[2], outline = "black", tags="playbutton")
-        #canvas.create_polygon(x1, y2, x1 + width/2, y2/2, x2, y2,fill=colores[3], outline = "black", tags="playbutton")
-        #canvas.create_rectangle(x1 + width/3, y1 + height/3, x2 - width/3, y2 - height/3,fill="white")
-        #canvas.tag_bind("d1_i","<Button-1>", clicked)
-        #canvas.tag_bind("d1_s","<Button-1>", clicked)
+        if (i%2):            
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2, fill="white", outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1, fill="white", outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2, fill="white", outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2, fill="white", outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+            canvas.create_oval(x1+5,y1+5,x2-5,y2-5, width=5, outline="blue")
 
-##        buttons.append(button)
-##        canvas.tag_bind(button, "<Button-1>", lambda event, index=i: button_click(event, index))
+        elif (i%3):                    
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2, fill="white", outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1, fill="white", outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2, fill="white", outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2, fill="white", outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+            canvas.create_line(x1+5,y1+5,x2-5,y2-5, fill="red", width=5)
+            canvas.create_line(x1+5,y2-5,x2-5,y1+5, fill="red", width=5)
+        else:
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2, fill=colores[0], outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1, fill=colores[1], outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2, fill=colores[2], outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2, fill=colores[3], outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+        x1=x2
+    canvas.create_line(0, y2+padding, ancho, y2+padding, width=3)
 
+    x1=x1+10
+    canvas.create_line(x1, 10, x1, 250, width=3)
+    for i in range(num_buttons):
+        x1 = x1 + padding
+        y1 = padding
+        x2 = x1 + width
+        y2 = y1 + height
+
+        if (i==5):            
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2, fill="white", outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1, fill="white", outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2, fill="white", outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2, fill="white", outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+            canvas.create_oval(x1+5, y1+5, x2-5, y2-5, width=5, outline="blue")
+
+        elif (i%3):                    
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2, fill="white", outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1, fill="white", outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2, fill="white", outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2, fill="white", outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+            canvas.create_line(x1+5, y1+5, x2-5, y2-5, fill="red", width=5)
+            canvas.create_line(x1+5, y2-5, x2-5, y1+5, fill="red", width=5)
+        else:
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2, fill=colores[0], outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1, fill=colores[1], outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2, fill=colores[2], outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2, fill=colores[3], outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+        x1=x2
+    y1=y2+20
+    x1=0
+    for i in range(num_buttons):
+        x1 = x1 + padding       
+        x2 = x1 + width
+        y2 = y1 + height                
+        if (i==1):            
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2, fill="white", outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1, fill="white", outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2, fill="white", outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2, fill="white", outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+            canvas.create_oval(x1+5, y1+5, x2-5, y2-5, width=5, outline="blue")
+
+        elif (i==3):                    
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2,fill="white", outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1,fill="white", outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2,fill="white", outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2,fill="white", outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+            canvas.create_line(x1+5,y1+5,x2-5,y2-5, fill="red", width=5)
+            canvas.create_line(x1+5,y2-5,x2-5,y1+5, fill="red", width=5)
+        else:
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2,fill=colores[0], outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1,fill=colores[1], outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2,fill=colores[2], outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2,fill=colores[3], outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+        x1=x2
+    x1=x1+10
+
+    for i in range(num_buttons):
+        x1 = x1 + padding       
+        x2 = x1 + width
+        y2 = y1 + height                
+        if (i==1):            
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2,fill="white", outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1,fill="white", outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2,fill="white", outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2,fill="white", outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+            canvas.create_oval(x1+5,y1+5,x2-5,y2-5, width=5, outline="blue")
+
+        elif (i==3):                    
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2,fill="white", outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1,fill="white", outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2,fill="white", outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2,fill="white", outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+            canvas.create_line(x1+5,y1+5,x2-5,y2-5, fill="red", width=5)
+            canvas.create_line(x1+5,y2-5,x2-5,y1+5, fill="red", width=5)
+        else:
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2,fill=colores[0], outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1,fill=colores[1], outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2,fill=colores[2], outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2,fill=colores[3], outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+        x1=x2
+    x1=155
+    y1=y2 + 50 
+    for i in range(num_buttons-3):
+        x1 = x1 + padding       
+        x2 = x1 + width
+        y2 = y1 + height                
+        if (i==1):            
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2, fill="white", outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1, fill="white", outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2, fill="white", outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2, fill="white", outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+            canvas.create_oval(x1+5, y1+5, x2-5, y2-5, width=5, outline="blue")
+
+        elif (i==3):                    
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2,fill="white", outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1,fill="white", outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2,fill="white", outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2,fill="white", outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+            canvas.create_line(x1+5,y1+5,x2-5,y2-5, fill="red", width=5)
+            canvas.create_line(x1+5,y2-5,x2-5,y1+5, fill="red", width=5)
+        else:
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2,fill=colores[0], outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1,fill=colores[1], outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2,fill=colores[2], outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2,fill=colores[3], outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+        x1=x2
+    x1=x2+11
+    y1= 165
+    for i in range(num_buttons-3):
+        x1 = x1 + padding       
+        x2 = x1 + width
+        y2 = y1 + height                
+        if (i==1):            
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2, fill="white", outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1, fill="white", outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2, fill="white", outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2, fill="white", outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+            canvas.create_oval(x1+5, y1+5, x2-5, y2-5, width=5, outline="blue")
+
+        elif (i==3):                    
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2,fill="white", outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1,fill="white", outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2,fill="white", outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2,fill="white", outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+            canvas.create_line(x1+5,y1+5,x2-5,y2-5, fill="red", width=5)
+            canvas.create_line(x1+5,y2-5,x2-5,y1+5, fill="red", width=5)
+        else:
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x1, y2,fill=colores[0], outline = "black")
+            canvas.create_polygon(x1, y1, x1 + width/2, y1 + height/2, x2, y1,fill=colores[1], outline = "black")
+            canvas.create_polygon(x2, y1, x1 + width/2, y1 + height/2, x2, y2,fill=colores[2], outline = "black")
+            canvas.create_polygon(x1, y2, x1 + width/2, y1 + height/2, x2, y2,fill=colores[3], outline = "black")
+            canvas.create_rectangle(x1 + width/3.0, y1 + height/3.0, x2 - width/3.0, y2 - height/3.0, fill="white")
+        x1=x2    
 crear_dientes()
 root.mainloop()
