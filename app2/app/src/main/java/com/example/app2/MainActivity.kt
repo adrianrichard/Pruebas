21db8c9aeb181d9c82a46ac09c2bf5751824c3ef
Adrian Richard <109044908+adrianrichard@users.noreply.github.com>
2025-03-30 23:16:18 -0300

    android
    comenzando a programar

---
+++ app2/app/src/main/java/com/example/app2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myfirstapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // Establece el diseño de la interfaz de usuario
+    }
+}
